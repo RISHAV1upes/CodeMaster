ae76c5afc89f5faf663a5879e7753fc1d32b69ff
Rishav Ranjan <rishavbits2016@gmail.com>
2018-02-25 17:51:41 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,29 @@
+<head>
+<link rel="stylesheet" href="css/maincss.css">
+</head>
+<html>
+<body>
+
+<!--footer section start-->
+
+<div class="bottom-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-7">
+                <p>©UPES-CSI. All Rights Reserved</p>
+            </div>
+            <div class="col-sm-12 col-md-5">
+                <ul class="social-media">
+                    <li class="social-icons"><a href="https://www.facebook.com/upescsi" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+                    <li class="social-icons"><a href="https://www.instagram.com/upescsi" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					<li class="social-icons"><a href="https://www.twitter.com/upescsi" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    
+                </ul>
+			</div>
+               
+		</div>
+	</div>
+	</div>
+	
+</body>
+</html>
